43c4c695827b9774cdf277aad04c00376e3633af
slinkybass <israel@garaballu.com>
2025-09-04 12:13:31 +0200

    Card and Set controllers

---
+++ src/Controller/Api/SetController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[Route('/api/{locale}/sets')]
+class SetController extends AbstractController
+{
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('', name: 'api_sets_list', methods: ['GET'])]
+    public function list(string $locale): JsonResponse
+    {
+        $response = $this->client->request('GET', "{$_ENV['TCG_BASE_API_URL']}/{$locale}/sets");
+        $data = $response->toArray();
+
+        return $this->json($data);
+    }
+
+    #[Route('/{id}', name: 'api_sets_get', methods: ['GET'])]
+    public function getCard(string $locale, string $id): JsonResponse
+    {
+        $response = $this->client->request('GET', "{$_ENV['TCG_BASE_API_URL']}/{$locale}/sets/{$id}");
+        $data = $response->toArray();
+
+        return $this->json($data);
+    }
+}
